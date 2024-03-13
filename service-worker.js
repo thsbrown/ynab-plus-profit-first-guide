0f21073a26946a9f433b539a8400c72a58e05aa1
Thomas <thsbrown1@gmail.com>
2024-03-12 20:34:28 -0700

    initial project file commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const YNAB_ORIGIN = 'https://app.ynab.com';
+
+// Allows users to open the side panel by clicking on the action toolbar icon
+chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch((error) => console.error(error));
+
+chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
+    if (!tab.url) {
+        return;
+    }
+    const url = new URL(tab.url);
+    // Enables the side panel on google.com
+    if (url.origin.startsWith(YNAB_ORIGIN)) {
+        await chrome.sidePanel.setOptions({
+            tabId,
+            path: 'sidepanel.html',
+            enabled: true
+        });
+    } else {
+        // Disables the side panel on all other sites
+        await chrome.sidePanel.setOptions({
+            tabId,
+            enabled: false
+        });
+    }
+});
