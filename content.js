1f131355ddab6690b8891bcbad6b4936c44c721f
Thomas <thsbrown1@gmail.com>
2024-10-01 16:43:07 -0700

    update content.js to only include whats necessary

--- content.js
+++ content.js
@@ -1,12 +1,4 @@
+//load twemoji on our document body
 let div = document.createElement('div');
 document.body.appendChild(div);
 twemoji.parse(document.body);
-let img = div.querySelector('img');
-
-// note the div is preserved
-img.parentNode === div; // true
-
-img.src;        // https://cdn.jsdelivr.net/gh/jdecked/twemoji@latest/assets/72x72/2764.png
-img.alt;        // \u2764\uFE0F
-img.className;  // emoji
-img.draggable;  // false
